ba71c5710f1c811f27cd0644e2798b2ca1766a72
anonymousrecords <a.Records2022@gmail.com>
2024-06-07 18:20:06 +0900

    feat : Loader 임시 변경

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,16 +1,16 @@
+'use client'
 import Image from "next/image";
 import { loaderElements } from "@/constants";
 
 const Loader = () => {
   const randomIndex = Math.floor(Math.random() * loaderElements.length + 1);
-  console.log(randomIndex);
 
   return (
     <div
-      className={`flex h-screen w-screen flex-col items-center justify-center gap-2 bg-${loaderElements[randomIndex].color}`}
+      className={`flex h-screen w-screen flex-col items-center justify-center gap-2 bg-white`}
     >
       <Image
-        src={`/loader/loader_${randomIndex}.gif`}
+        src={`/loader/loader_1.gif`}
         alt={"loader"}
         width={500}
         height={500}
